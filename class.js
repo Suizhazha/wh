7904b32235960af9c52176ff80c5a774837625d1
Suizhazha <573505403@qq.com>
2021-01-05 22:01:56 +0800

    class

--- class.js
+++ class.js
@@ -1,4 +1,4 @@
-class dog {
+class Dog {
   constructor(name,age) {
     this.name   = name
     this.age = age
@@ -9,10 +9,8 @@ class dog {
   }
 }
 
-let dog1 = new dog('wuhao',23)
+let dog1 = new Dog('wuhao',23)
 
-dog1.age
-dog1.name
 dog1.say()
 
 console.log(dog1.hasOwnProperty('name'),
@@ -20,3 +18,20 @@ dog1.hasOwnProperty('age'),
 dog1.hasOwnProperty('say'),
 dog1.__proto__.hasOwnProperty('say'))
 
+class dogSon extends Dog {
+  constructor(action) {
+
+    // 子类必须要在constructor中指定super 函数，否则在新建实例的时候会报错.
+    // 如果没有置顶constructor,默认带super函数的constructor将会被添加、
+
+    super('dog2','23');
+    this.action = action
+  }
+  say() {
+    console.log(super.action)
+  }
+}
+
+let dog2 = new dogSon('1')
+
+dog2.say()
